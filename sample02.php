e945d941d80574d024b38d752a3c9c838f4a67fa
limea <tekitoukunn9@gmail.com>
2015-05-13 04:36:25 +0000

    sample01

---
+++ sample02.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print('<h2 style="font-size: 110%; font-weight: bold">画面
+    にHTMLを表示する</h2>');
+    print('<p>PHPはHTMLの中に埋め込んで記述することができます。</p>');
+    print('<p>改行するときは、<br/>brタグで記述します</p>');
+    ?>
+</body>
+</html>
---
+++ sample04.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="sample04.php" method="get">
+        <label for="my_name">お名前:</label>
+        <input type="text" name=""/>
+    </form>
+    
+</body>
+</html>
